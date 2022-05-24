2132409f36c1d1abd9965eef85b4342d8de1f99b
klkl <kuanglong0312@sina.com>
2022-05-24 10:00:31 +0800

    feat: click block get the dom and selector

--- src/preload.js
+++ src/preload.js
@@ -9,13 +9,16 @@ window.onload = () => {
         break
       case 'Overlay.inspectNodeRequested':
         let nodeId = null
-        if (message.params?.nodeId) {
+        if (message.params.nodeId) {
           nodeId = message.params.nodeId
-        } else if (message.params?.backendNodeId) {
+        } else if (message.params.backendNodeId) {
           nodeId = message.params.backendNodeId
+          message.params.nodeId = message.params.backendNodeId
         }
         const dom = chobitsu.domain('DOM').getNodeByNodeId(nodeId)
+        const selector = chobitsu.domain('CSS').getCssSelectorForNode(message.params)
         console.log(dom)
+        console.log(selector)
         break
     }
   })
@@ -29,187 +32,4 @@ window.onload = () => {
       marginColor: 'rgba(246, 178, 107, .66)',
     }
   })
-
-  const btn = document.createElement('button')
-  btn.innerText = 'test'
-  btn.style.position = 'absolute'
-  btn.style.top = '20px'
-  btn.style.left = '20px'
-  btn.style.zIndex = '999999'
-  document.body.appendChild(btn)
-  btn.addEventListener('click', () => {
-    window.ipcRenderer.send('test', 'test')
-  })
-
-  const domMap = new Map()
-  document.body.addEventListener('click', function (event) {
-    const element = event.target
-    console.log('chrome', cssPath(element, true))
-    element.classList.add('active')
-    domMap.set(cssPath(element, true), element.innerText)
-  })
-
-  document.body.addEventListener('mousemove', function (event) {
-    const element = event.target
-  }, {
-    capture: true
-  })
-
-  document.body.addEventListener('mouseleave', function (event) {
-    const element = event.target
-  }, {
-    capture: true
-  })
-
-  class Step {
-    constructor(value, optimized) {
-      this.value = value
-      this.optimized = optimized || false
-    }
-
-    toString() {
-      return this.value
-    }
-  }
-  const cssPath = function (node, optimized) {
-    if (node.nodeType !== Node.ELEMENT_NODE) {
-      return ''
-    }
-
-    const steps = []
-    let contextNode = node
-    while (contextNode) {
-      const step = cssPathStep(contextNode, Boolean(optimized), contextNode === node)
-      if (!step) {
-        break
-      } // Error - bail out early.
-      steps.push(step)
-      if (step.optimized) {
-        break
-      }
-      contextNode = contextNode.parentNode
-    }
-
-    steps.reverse()
-    return steps.join(' > ')
-  }
-  const cssPathStep = function (node, optimized, isTargetNode) {
-    if (node.nodeType !== Node.ELEMENT_NODE) {
-      return null
-    }
-
-    const id = node.getAttribute('id')
-    if (optimized) {
-      if (id) {
-        return new Step(idSelector(id), true)
-      }
-      const nodeNameLower = node.nodeName.toLowerCase()
-      if (nodeNameLower === 'body' || nodeNameLower === 'head' || nodeNameLower === 'html') {
-        return new Step(nodeNameInCorrectCase(node), true)
-      }
-    }
-    const nodeName = nodeNameInCorrectCase(node)
-
-    if (id) {
-      return new Step(nodeName + idSelector(id), true)
-    }
-    const parent = node.parentNode
-    if (!parent || parent.nodeType === Node.DOCUMENT_NODE) {
-      return new Step(nodeName, true)
-    }
-
-    function prefixedElementClassNames(node) {
-      const classAttribute = node.getAttribute('class')
-      if (!classAttribute) {
-        return []
-      }
-
-      return classAttribute.split(/\s+/g).filter(Boolean).map(function (name) {
-        // The prefix is required to store "__proto__" in a object-based map.
-        return '$' + name
-      })
-    }
-
-    function idSelector(id) {
-      return '#' + CSS.escape(id)
-    }
-
-    const prefixedOwnClassNamesArray = prefixedElementClassNames(node)
-    let needsClassNames = false
-    let needsNthChild = false
-    let ownIndex = -1
-    let elementIndex = -1
-    const siblings = parent.children
-    for (let i = 0; siblings && (ownIndex === -1 || !needsNthChild) && i < siblings.length; ++i) {
-      const sibling = siblings[i]
-      if (sibling.nodeType !== Node.ELEMENT_NODE) {
-        continue
-      }
-      elementIndex += 1
-      if (sibling === node) {
-        ownIndex = elementIndex
-        continue
-      }
-      if (needsNthChild) {
-        continue
-      }
-      if (nodeNameInCorrectCase(sibling) !== nodeName) {
-        continue
-      }
-
-      needsClassNames = true
-      const ownClassNames = new Set(prefixedOwnClassNamesArray)
-      if (!ownClassNames.size) {
-        needsNthChild = true
-        continue
-      }
-      const siblingClassNamesArray = prefixedElementClassNames(sibling)
-      for (let j = 0; j < siblingClassNamesArray.length; ++j) {
-        const siblingClass = siblingClassNamesArray[j]
-        if (!ownClassNames.has(siblingClass)) {
-          continue
-        }
-        ownClassNames.delete(siblingClass)
-        if (!ownClassNames.size) {
-          needsNthChild = true
-          break
-        }
-      }
-    }
-
-    let result = nodeName
-    if (isTargetNode && nodeName.toLowerCase() === 'input' && node.getAttribute('type') && !node.getAttribute('id') &&
-      !node.getAttribute('class')) {
-      result += '[type=' + CSS.escape((node.getAttribute('type')) || '') + ']'
-    }
-    if (needsNthChild) {
-      result += ':nth-child(' + (ownIndex + 1) + ')'
-    } else if (needsClassNames) {
-      for (const prefixedName of prefixedOwnClassNamesArray) {
-        result += '.' + CSS.escape(prefixedName.slice(1))
-      }
-    }
-
-    return new Step(result, false)
-  }
-
-  function nodeNameInCorrectCase(node) {
-    const shadowRootType = node.shadowRootType
-    if (shadowRootType) {
-      return '#shadow-root (' + shadowRootType + ')'
-    }
-
-    // If there is no local #name, it's case sensitive
-    if (!node.localName) {
-      return node.nodeName
-    }
-
-    // If the names are different lengths, there is a prefix and it's case sensitive
-    if (node.localName.length !== node.nodeName.length) {
-      return node.nodeName
-    }
-
-    // Return the localname, which will be case insensitive if its an html node
-    return node.localName
-  }
 }
